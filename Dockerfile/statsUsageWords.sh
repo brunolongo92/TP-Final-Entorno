da1475aedfc58ba8f2dfc2438ae0a62babc501b8
Bruno Longo <94638303+brunolongo92@users.noreply.github.com>
2022-07-29 11:59:31 -0300

    Update statsUsageWords.sh

--- Dockerfile/statsUsageWords.sh
+++ Dockerfile/statsUsageWords.sh
@@ -3,7 +3,7 @@
 touch salida.txt
 touch salida2.txt
 touch salida3.txt
-touch salida4.txt
+touch salida4.txt   #archivos auxiliares
 
 TEXTO=$(cat $1)
 
@@ -11,22 +11,22 @@ for palabra in $TEXTO
 do
   if [ ${#palabra} -gt 3 ]
   then
-  echo $palabra | tr -d [".",","] >> salida.txt
-  grep -o -i $palabra salida.txt | wc -l >> salida2.txt
+  echo $palabra | tr -d [".",",",":","-"] >> salida.txt   #remueve signos ortográficos usuales
+  grep -o -i $palabra salida.txt | wc -l >> salida2.txt   #guarda la cantidad de caracteres por palabra
   fi
 done
 
-(sort --reverse <(paste -d ":" salida2.txt salida.txt)) >> salida3.txt
+(sort --reverse <(paste -d ":" salida2.txt salida.txt)) >> salida3.txt  #lista con forma "palabra":"#caracteres"
 
 FILE=$(cat salida.txt)
 FILE2=$(cat salida3.txt)
 
 for palabra in $FILE
 do
-  grep $palabra salida3.txt | head -1 >> salida4.txt
+  grep $palabra salida3.txt | head -1 >> salida4.txt    #filtra los valores acumulados de salida3.txt
 done
 
 echo "Cantidad y nombre de las 10 palabras mas usadas en el texto:"
-sort --uniq --reverse salida4.txt | head -n 10
+sort --uniq --reverse salida4.txt | head -n 10          #selecciona los 10 mayores de salida4.txt
 
 rm salida.txt salida2.txt salida3.txt salida4.txt 
