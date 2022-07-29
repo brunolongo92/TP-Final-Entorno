a6dcb94c68db61db25c5d2b4e3d8b1ae1c02fff9
Bruno Longo <94638303+brunolongo92@users.noreply.github.com>
2022-07-29 12:03:01 -0300

    Update findNames.sh

--- Dockerfile/findNames.sh
+++ Dockerfile/findNames.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-touch salida.txt salida2.txt
+touch salida.txt salida2.txt   #archivos auxiliares
 
 TEXTO=$(cat $1)
 FILE=$(cat salida.txt)
@@ -9,12 +9,12 @@ for palabra in $TEXTO
 do 
   if [ ${#palabra} -gt 3 ]
   then
-  echo $palabra | tr -d [".",","] >> salida.txt
-  grep -o '\<[A-Z][a-z]*\>' salida.txt >> salida2.txt
+  echo $palabra | tr -d [".",","] >> salida.txt     #remueve signos ortográficos usuales
+  grep -o '\<[A-Z][a-z]*\>' salida.txt >> salida2.txt   #filtra con expresión regular que inicie con MAYUSC
   fi
 done
 
 echo "Las palabras en mayúscula del texto son:"
-sort --uniq salida2.txt
+sort --uniq salida2.txt   #filtra repetidos y ordena alfabeticamente
 
 rm salida.txt salida2.txt
