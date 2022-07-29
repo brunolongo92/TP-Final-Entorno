f0b917b07769249b0015e6de39ed110bf4bb566b
Bruno Longo <94638303+brunolongo92@users.noreply.github.com>
2022-07-29 12:03:39 -0300

    Update blankLinesCounter.sh

--- Dockerfile/blankLinesCounter.sh
+++ Dockerfile/blankLinesCounter.sh
@@ -2,4 +2,4 @@
 
 FILE=$1
 
-echo "Las lineas en blanco en el archivo son:" ; grep -cvP '\S' $1 
+echo "Las lineas en blanco en el archivo son:" ; grep -cvP '\S' $1 #filtra cantidad de lineas en blanco
